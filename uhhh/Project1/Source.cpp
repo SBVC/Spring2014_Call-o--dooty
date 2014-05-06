7c2546b0eb63a07941ec4e95706cc77783dd501c
Ngara <ngarabird@yahoo.com>
2014-05-06 10:19:17 -0700

    Update Source.cpp

--- uhhh/Project1/Source.cpp
+++ uhhh/Project1/Source.cpp
@@ -25,13 +25,27 @@ cout << "The man appears to be dead from a wound in his back. You find a folded 
 cout << "You read the paper:\n"
 cout << "1721\n"
         
-cout << "I do not remember how many days I have spent on this wretched ship. My captain was sleeping in his quarters\n"
-cout << "when a ship appeared on the horizon. As it drew closer, it raised a black flag."
+cout << "I do not remember how many days I have spent on this wretched ship.\n"
+cout << "I remember before this nightmare began...My captain was sleeping in his quarters\n"
+cout << "when a ship appeared on the horizon. As it drew closer, it raised a black flag.\n"
+cout << "'Pirates!' I thundered to the crew."
 cout << "The crew started shouting and preparing to outmaneouver the ship on the horizon.\n"
 cout << "I ran to the captain's quarters and burst through the old wooden door.\n"
 cout << "I saw the captain sprawled out on his bed, a bottle of rum rolled out of his hand and pattered to the floor.\n"
 cout << "The sound of the crew's panic faded from my mind as the captain wheezed out his final words.\n"
 cout << "'I have been poisoned. Trust no one.'\n"
+cout << "My legs grew weak, but I knew what I had to do. I ran on the the deck and began to shout orders.\n"
+cout << "'Where is the captain?' I heard one of the crewmembers shout.\n"
+cout << "'The captain is dead. Someone has poisoned him. As your quartermaster I am now in command.'\n"
+cout << "I could see the questions forming in their eyes. Regardless, they kept working to escape from the pirate ship.\n"
+cout << "Our ship was much slower than theirs and by nightfall they were almost at our side.\n"
+cout << "I heard a voice from beside me. It was the navigator-"
+
+cout << "You drop the entry onto the sand and fall to your knees. The sounds of the waves become disorted and\n"
+cout << "you can no longer see clearly. Your head hits the ground. You catch a glimpse of a dark figure standing above you\n"
+cout << "before your eyes close and you lose consciousness.\n"
+
+
         
         
 
